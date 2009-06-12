d34533302b5091594595766829587448e9fb0328
vogelwd <vogelwd@4a434eb8-3ccc-11de-a9fe-ad1e22fc3a8a>
2009-06-12 01:38:45 +0000

    Se agrego la funcionalidad para seleccionar la region de interes

--- trunk/Src/qimagepage.cpp
+++ trunk/Src/qimagepage.cpp
@@ -6,27 +6,104 @@
 
 QImagePage::QImagePage(QWidget* parent) : QWidget(parent)
 {
-    this->image = new QImage(500,500,QImage::Format_RGB32);
-    this->image->fill(qRgb(255,255,255));
+    this->image = new OsteoporosisImage(QSize(500,500));
+    this->board = new QImage(500,500,QImage::Format_ARGB32);
+    this->board->fill(qRgba(0,0,0,0));
     this->setMinimumSize(500,500);
     this->mainWindow = parent;
+    this->currentAction = NOTHING;
+    this->currentState = UNDEFINED;
 }
 
-QImagePage::QImagePage(QImage* image, QWidget* parent) : QWidget(parent)
+QImagePage::QImagePage(OsteoporosisImage* image, QWidget* parent) : QWidget(parent)
 {
     this->image = image;
-    this->setMinimumSize(image->width(),image->height());
+    this->setMinimumSize(image->getWidth(),image->getHeight());
+    this->board = new QImage(image->getWidth(),image->getHeight(),QImage::Format_ARGB32);
+    this->board->fill(qRgba(0,0,0,0));
     this->mainWindow = parent;
+    this->currentAction = NOTHING;
+    this->currentState = UNDEFINED;
 }
 
 void QImagePage::paintEvent(QPaintEvent* )
 {
     QPainter painter(this);
-    painter.drawImage(QPoint(0,0),*this->image);
+    painter.drawImage(QPoint(0,0),*(this->image->getImage()));
+    painter.drawImage(QPoint(0,0),*(this->board));
 }
 
 void QImagePage::mousePressEvent(QMouseEvent* event)
 {
-    //this->mainWindow->mousePressEvent(event)
+    switch (this->currentAction)
+    {
+        case NOTHING: break;
+        case SEL_REGION:{
+                        if (this->currentState == UNDEFINED){
+                            this->points.push_back(event->pos());
+                            this->points.push_back(event->pos());
+                            this->currentState = DRAWING;
+                        }
+                    } break;
+        case SEL_TWARD: break;
+        case SEL_CP: break;
+    }
 }
 
+void QImagePage::mouseMoveEvent(QMouseEvent* event)
+{
+    switch (this->currentAction)
+    {
+        case NOTHING: break;
+        case SEL_REGION:{
+                        if (this->currentState == DRAWING){
+                            this->points.pop_back();
+                            this->points.push_back(event->pos());
+                            this->draw();
+                        }
+                    } break;
+        case SEL_TWARD: break;
+        case SEL_CP: break;
+    }
+    update();
+}
+
+void QImagePage::mouseReleaseEvent(QMouseEvent* event)
+{
+    switch (this->currentAction)
+    {
+        case NOTHING: break;
+        case SEL_REGION:{
+                        this->currentState = FINISHED;
+                    } break;
+        case SEL_TWARD: break;
+        case SEL_CP: break;
+    }
+}
+
+OsteoporosisImage* QImagePage::getImage()
+{
+    return this->image;
+}
+
+void QImagePage::setAction(action a)
+{
+    this->currentAction = a;
+}
+
+void QImagePage::draw()
+{
+    switch (this->currentAction)
+    {
+        case NOTHING: break;
+        case SEL_REGION:{
+                        this->board->fill(qRgba(0,0,0,0));
+                        QPainter painter(board);
+                        painter.setBrush(QBrush(Qt::red, Qt::BDiagPattern));
+                        painter.setPen(QPen(Qt::red, 1, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+                        painter.drawRect(QRect(this->points.first(),this->points.last()));
+                    } break;
+        case SEL_TWARD: break;
+        case SEL_CP: break;
+    }
+}
