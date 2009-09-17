c3b7a5ec3ca0f5b560289e07cdfe60d7a10d95db
Arturo.F.Iglesias <Arturo.F.Iglesias@4a434eb8-3ccc-11de-a9fe-ad1e22fc3a8a>
2009-09-17 18:15:08 +0000

    se agrego el inform

--- Src/ui_mainwindow.h
+++ Src/ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading ui file 'mainwindow.ui'
 **
-** Created: Thu Sep 17 14:34:34 2009
+** Created: Thu Sep 17 14:55:23 2009
 **      by: Qt User Interface Compiler version 4.5.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
