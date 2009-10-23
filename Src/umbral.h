ef26f7d6044dcdfe1052f2c9082269c697002c8f
vogelwd <vogelwd@4a434eb8-3ccc-11de-a9fe-ad1e22fc3a8a>
2009-10-23 17:14:34 +0000

    Se hicieron limpieza de archivos y arreglos

---
+++ Src/umbral.h
@@ -0,0 +1,19 @@
+#include "transformation.h"
+
+#ifndef UMBRAL_H
+#define UMBRAL_H
+
+class Umbral : public Transformation
+{
+private:
+    int value;
+
+public:
+    Umbral(int value);
+
+    void setUmbral(int value);
+    int getUmbral();
+    class OsteoporosisImage* apply(class OsteoporosisImage* image);
+};
+
+#endif // UMBRAL_H
