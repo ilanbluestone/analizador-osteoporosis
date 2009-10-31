c6c939ba8f1d6c2bc545e90307678b5d3e978ce7
Arturo.F.Iglesias <Arturo.F.Iglesias@4a434eb8-3ccc-11de-a9fe-ad1e22fc3a8a>
2009-10-31 15:43:25 +0000

    resulve indece de osteoporosis

---
+++ Src/osteoporosistexture.cpp
@@ -0,0 +1,71 @@
+#include "osteoporosistexture.h"
+#include "math.h"
+OsteoporosisTexture::OsteoporosisTexture(OsteoporosisImage* i)
+{
+        image=i;
+}
+
+void OsteoporosisTexture::binarize()
+{
+    binarizeImage=new OsteoporosisImage(this->image->getSize());
+    int gray=this->grayAverage();
+    for (int i=0; i<this->image->getWidth();i++)
+        for(int j=0; j<this->image->getHeight();i++)
+            if (this->image->getColorAt(i,j)< gray)
+                binarizeImage->setColorAt(i,j,0);
+            else
+                binarizeImage->setColorAt(i,j,1);
+}
+
+float OsteoporosisTexture::calculateCosHor(int i)
+{
+    float result=0;
+    for (int j=0;j<this->binarizeImage->getWidth();j++)
+        result+=this->binarizeImage->getColorAt(i,j);
+    return (result/(sqrt(result)*sqrt(this->image->getWidth())));
+}
+
+float OsteoporosisTexture::calculateCosVer(int j)
+{
+    float result=0;
+    for (int i=0;j<this->binarizeImage->getHeight();i++)
+        result+=this->binarizeImage->getColorAt(i,j);
+    return (result/(sqrt(result)*sqrt(this->image->getWidth())));
+}
+
+float OsteoporosisTexture::cosAverage(QVector<float> a)
+{
+    float result=0;
+    for (int i=0; i< a.size();i++)
+        result+=a.at(i);
+    return (result/a.size());
+}
+
+void OsteoporosisTexture::createHorArray()
+{
+    for (int i=0;i<this->image->getHeight();i++)
+        this->horizontal.push_front(this->calculateCosHor(i));
+}
+
+void OsteoporosisTexture::createVerArray()
+{
+      for (int j=0;j<this->image->getWidth();j++)
+        this->vertical.push_front(calculateCosVer(j));
+}
+
+float OsteoporosisTexture::grayAverage()
+{
+    float result=0;
+    for(int i=0; i<this->image->getWidth();i++)
+        for (int j=0; j<this->image->getHeight();j++)
+            result+=this->image->getColorAt(i,j);
+    return (result/(this->image->getWidth()*this->image->getHeight()));
+}
+
+float OsteoporosisTexture::indexCalculate()
+{
+    this->binarize();
+    this->createHorArray();
+    this->createVerArray();
+    return ((this->cosAverage(this->horizontal) + this->cosAverage(this->vertical))/2);
+}
---
+++ Src/osteoporosistexture.h
@@ -0,0 +1,35 @@
+#ifndef OSTEOPOROSISTEXTURE_H
+#define OSTEOPOROSISTEXTURE_H
+#include "osteoporosisimage.h"
+#include<QVector>
+
+class OsteoporosisTexture
+{
+private:
+        OsteoporosisImage* image;
+        QVector<float> horizontal;
+        QVector<float> vertical;
+        OsteoporosisImage* binarizeImage;
+public:
+
+    OsteoporosisTexture(OsteoporosisImage* i);
+    // saca el promedio de gris de la imagen para poder binarizarla
+    float grayAverage();
+    // Bianriza la imagen dado el promedio de gris
+    void binarize();
+    //Crea el arreglo horizantal con los cosenos de las filas
+    void createHorArray();
+    //Crea el arreglo vertical con los cosenos de las columnas
+    void createVerArray();
+    //Calcula el coseno de una fila
+    float calculateCosHor(int i);
+    //Calcua el coseno de una columna
+    float calculateCosVer(int j);
+    //saca el promedio de los cosenos
+    float cosAverage(QVector<float>);
+    //Calcuala el indice de osteoporosis
+    float indexCalculate();
+
+};
+
+#endif // OSTEOPOROSISTEXTURE_H
